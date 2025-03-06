20181d3471163880b5297e90bcc4d97a2104e520
NJ <naveenkumar.jangir@cognizant.com>
2025-03-06 05:53:51 +0530

    updated display of events, news and posts

--- src/App.js
+++ src/App.js
@@ -16,17 +16,18 @@ const App = () => {
       { name: 'Contact', logo: require('./images/Home/contact.jpg'), isSelected: false }
     ],
     posts: [
-      { title: 'Post 1', content: 'Content 1', date: '01 Jun 2025' },
-      { title: 'Post 2', content: 'Content 2', date: '02 Jun 2025' },
-      { title: 'Post 3', content: 'Content 3', date: '03 Jun 2025' },
-      { title: 'Post 4', content: 'Content 4', date: '04 Jun 2025' },
-      { title: 'Post 5', content: 'Content 5', date: '05 Jun 2025' },
-      { title: 'Post 6', content: 'Content 6', date: '06 Jun 2025' },
-      { title: 'Post 7', content: 'Content 7', date: '07 Jun 2025' },
-      { title: 'Post 8', content: 'Content 8', date: '08 Jun 2025' },
-      { title: 'Post 9', content: 'Content 9', date: '09 Jun 2025' },
-      { title: 'Post 10', content: 'Content 10', date: '10 Jun 2025' }
+      { id: 1, title: 'Post 1', content: 'Content 1', date: '01 Jun 2025' },
+      { id: 2, title: 'Post 2', content: 'Content 2', date: '02 Jun 2025' },
+      { id: 3, title: 'Post 3', content: 'Content 3', date: '03 Jun 2025' },
+      { id: 4, title: 'Post 4', content: 'Content 4', date: '04 Jun 2025' },
+      { id: 5, title: 'Post 5', content: 'Content 5', date: '05 Jun 2025' },
+      { id: 6, title: 'Post 6', content: 'Content 6', date: '06 Jun 2025' },
+      { id: 7, title: 'Post 7', content: 'Content 7', date: '07 Jun 2025' },
+      { id: 8, title: 'Post 8', content: 'Content 8', date: '08 Jun 2025' },
+      { id: 9, title: 'Post 9', content: 'Content 9', date: '09 Jun 2025' },
+      { id: 10, title: 'Post 10', content: 'Content 10', date: '10 Jun 2025' }
     ],
+    selectedPost: '',
     homePageLinks: [
       { name: 'Professional Developement', logo: require('./images/Home/pd.jpg'), isSelected: false },
       { name: 'Student Instruction', logo: require('./images/Home/si.jpg'), isSelected: false },
@@ -40,29 +41,31 @@ const App = () => {
       { name: 'Image Gallery', isSelected: false }
     ],
     events: [
-      { title: 'Event 1', content: 'Content 1', date: '01 Jun 2025' },
-      { title: 'Event 2', content: 'Content 2', date: '02 Jun 2025' },
-      { title: 'Event 3', content: 'Content 3', date: '03 Jun 2025' },
-      { title: 'Event 4', content: 'Content 4', date: '04 Jun 2025' },
-      { title: 'Event 5', content: 'Content 5', date: '05 Jun 2025' },
-      { title: 'Event 6', content: 'Content 6', date: '06 Jun 2025' },
-      { title: 'Event 7', content: 'Content 7', date: '07 Jun 2025' },
-      { title: 'Event 8', content: 'Content 8', date: '08 Jun 2025' },
-      { title: 'Event 9', content: 'Content 9', date: '09 Jun 2025' },
-      { title: 'Event 10', content: 'Content 10', date: '10 Jun 2025' }
+      { id: 1, title: 'Event 1', content: 'Content 1', date: '01 Jun 2025' },
+      { id: 2, title: 'Event 2', content: 'Content 2', date: '02 Jun 2025' },
+      { id: 3, title: 'Event 3', content: 'Content 3', date: '03 Jun 2025' },
+      { id: 4, title: 'Event 4', content: 'Content 4', date: '04 Jun 2025' },
+      { id: 5, title: 'Event 5', content: 'Content 5', date: '05 Jun 2025' },
+      { id: 6, title: 'Event 6', content: 'Content 6', date: '06 Jun 2025' },
+      { id: 7, title: 'Event 7', content: 'Content 7', date: '07 Jun 2025' },
+      { id: 8, title: 'Event 8', content: 'Content 8', date: '08 Jun 2025' },
+      { id: 9, title: 'Event 9', content: 'Content 9', date: '09 Jun 2025' },
+      { id: 10, title: 'Event 10', content: 'Content 10', date: '10 Jun 2025' }
     ],
+    selectedEvent: '',
     headlines: [
-      { title: 'News 1', content: 'Content 1', date: '01 Jun 2025' },
-      { title: 'News 2', content: 'Content 2', date: '02 Jun 2025' },
-      { title: 'News 3', content: 'Content 3', date: '03 Jun 2025' },
-      { title: 'News 4', content: 'Content 4', date: '04 Jun 2025' },
-      { title: 'News 5', content: 'Content 5', date: '05 Jun 2025' },
-      { title: 'News 6', content: 'Content 6', date: '06 Jun 2025' },
-      { title: 'News 7', content: 'Content 7', date: '07 Jun 2025' },
-      { title: 'News 8', content: 'Content 8', date: '08 Jun 2025' },
-      { title: 'News 9', content: 'Content 9', date: '09 Jun 2025' },
-      { title: 'News 10', content: 'Content 10', date: '10 Jun 2025' }
+      { id: 1, title: 'News 1', content: 'Content 1', date: '01 Jun 2025' },
+      { id: 2, title: 'News 2', content: 'Content 2', date: '02 Jun 2025' },
+      { id: 3, title: 'News 3', content: 'Content 3', date: '03 Jun 2025' },
+      { id: 4, title: 'News 4', content: 'Content 4', date: '04 Jun 2025' },
+      { id: 5, title: 'News 5', content: 'Content 5', date: '05 Jun 2025' },
+      { id: 6, title: 'News 6', content: 'Content 6', date: '06 Jun 2025' },
+      { id: 7, title: 'News 7', content: 'Content 7', date: '07 Jun 2025' },
+      { id: 8, title: 'News 8', content: 'Content 8', date: '08 Jun 2025' },
+      { id: 9, title: 'News 9', content: 'Content 9', date: '09 Jun 2025' },
+      { id: 10, title: 'News 10', content: 'Content 10', date: '10 Jun 2025' }
     ],
+    selectedHeadline: '',
     courses: [
       { name: 'Basic Courses', logo: require('./images/Courses/basic.jpg'), isSelected: false },
       { name: 'Special Courses', logo: require('./images/Courses/special.jpg'), isSelected: false }
@@ -128,6 +131,36 @@ const App = () => {
               }
             })
           };
+        case 'DISPLAY_EVENT':
+          return {
+            ...state,
+            selectedEvent: state.events.find(event => event.id === action.id)
+          }
+        case 'CLOSE_EVENT':
+          return {
+            ...state,
+            selectedEvent: ''
+          }
+        case 'DISPLAY_HEADLINE':
+          return {
+            ...state,
+            selectedHeadline: state.headlines.find(headline => headline.id === action.id)
+          }
+        case 'CLOSE_HEADLINE':
+          return {
+            ...state,
+            selectedHeadline: ''
+          }
+        case 'DISPLAY_POST':
+          return {
+            ...state,
+            selectedPost: state.posts.find(post => post.id === action.id)
+          }
+        case 'CLOSE_POST':
+          return {
+            ...state,
+            selectedPost: ''
+          }
       default:
         return state;
     }
--- src/pages/admissions/admissions.js
+++ src/pages/admissions/admissions.js
@@ -5,9 +5,10 @@ import IMAGE3 from '../../images/Slider/image03.jpg';
 import IMAGE4 from '../../images/Slider/image04.jpg';
 import IMAGE5 from '../../images/Slider/image05.jpg';
 import ADMISSIONS01 from '../../images/Admissions/admissions01.jpg';
+import ClOSE from '../../images/close.png';
 import './admissions.css';
 
-const Admissions = ({ state }) => {
+const Admissions = ({ state, dispatch }) => {
   const images = [
     { name: IMAGE1, isSelected: false },
     { name: IMAGE2, isSelected: false },
@@ -45,31 +46,48 @@ const Admissions = ({ state }) => {
       <div className='admissions-right'>
         <label>Events</label>
         <div className='events'>
-          <div className='scroll-events'>
-            {
-              state.events.slice(5).reverse().map((event, i) => (
-                <div key={i} className='events-item'>
-                  <div style={{fontWeight: 'bolder'}}>{event.date}</div>
-                  <p>{event.title}</p>
-                  <div style={{fontSize: 'x-small'}}>{event.content}</div>
-                </div>
-              ))
-            }
-          </div>
+          {
+            state.selectedEvent === '' ?
+            <div className='scroll-events'>
+              {
+                state.events.slice(5).reverse().map((event, i) => (
+                  <div key={i} className='events-item' onClick={() => dispatch({type: 'DISPLAY_EVENT', id: event.id})}>
+                    <div style={{fontWeight: 'bolder'}}>{event.date}</div>
+                    <p>{event.title}</p>
+                  </div>
+                ))
+              }
+            </div> :
+            <div className='event-card'>
+              <img className='close' src={ClOSE} alt='close' onClick={() => dispatch({type: 'CLOSE_EVENT'})} />
+              <h4>{state.selectedEvent.date}</h4>
+              <p>{state.selectedEvent.title}</p>
+              <div>{state.selectedEvent.content}</div>
+            </div>
+          }
         </div>
         <label>News</label>
         <div className='news'>
-          <div className='scroll-news'>
-            {
-              state.headlines.slice(5).reverse().map((news, i) => (
-                <div key={i} className='news-item'>
-                  <div style={{fontWeight: 'bolder'}}>{news.date}</div>
-                  <p>{news.title}</p>
-                  <div style={{fontSize: 'x-small'}}>{news.content}</div>
-                </div>
-              ))
-            }
-          </div>
+          {
+            state.selectedHeadline === '' ? 
+            <div className='scroll-news'>
+              {
+                state.headlines.slice(5).reverse().map((news, i) => (
+                  <div key={i} className='news-item' onClick={() => dispatch({type: 'DISPLAY_HEADLINE', id: news.id})}>
+                    <div style={{fontWeight: 'bolder'}}>{news.date}</div>
+                    <p>{news.title}</p>
+                    <div style={{fontSize: 'x-small'}}>{news.content}</div>
+                  </div>
+                ))
+              }
+            </div> :
+            <div className='news-card'>
+              <img className='close' src={ClOSE} alt='close' onClick={() => dispatch({type: 'CLOSE_HEADLINE'})} />
+              <h4>{state.selectedHeadline.date}</h4>
+              <p>{state.selectedHeadline.title}</p>
+              <div>{state.selectedHeadline.content}</div>
+            </div>
+          }
         </div>
         <label>Gallery</label>
         <div className='gallery'>
