a40efb2224568fedb7ab0811e43774b4e818291d
NJ <naveenkumar.jangir@cognizant.com>
2025-03-12 01:57:41 +0530

    fixed mobile view for time tables

--- src/pages/tenth/tenth.js
+++ src/pages/tenth/tenth.js
@@ -6,89 +6,152 @@ const Tenth = ({ state, dispatch }) => {
     <div className='container'>
       <div className='container-left'>
         <div className='title'>Time Table for Class 8<sup>th</sup>, 9<sup>th</sup> and 10<sup>th</sup></div>
-        <div className='tenth'>
-          <div className='col'>
-            <div className='row'>Class</div>
-            <div className='row'>8th</div>
-            <div className='row'>8th</div>
-            <div className='row'>9th</div>
-            <div className='row'>9th</div>
-            <div className='row'>10th</div>
-            <div className='row'>10th</div>
+          <div className='tenth'>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 8th</div>
+                <div className='cell'>Time 5:00 - 6:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 8th</div>
+                <div className='cell'>Time 6:00 - 7:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>SCI</div>
+                <div className='cell'>SCI</div>
+                <div className='cell'>SCI</div>
+                <div className='cell'>SCI</div>
+                <div className='cell'>SCI</div>
+                <div className='cell'>SCI</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 9th</div>
+                <div className='cell'>Time 5:00 - 6:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 9th</div>
+                <div className='cell'>Time 6:00 - 7:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>BIO</div>
+                <div className='cell'>BIO</div>
+                <div className='cell'>CHE</div>
+                <div className='cell'>CHE</div>
+                <div className='cell'>PHY</div>
+                <div className='cell'>PHY</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 10th</div>
+                <div className='cell'>Time 5:00 - 6:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>BIO</div>
+                <div className='cell'>BIO</div>
+                <div className='cell'>CHE</div>
+                <div className='cell'>CHE</div>
+                <div className='cell'>PHY</div>
+                <div className='cell'>PHY</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='row'>
+                <div className='cell'>Class 10th</div>
+                <div className='cell'>Time 6:00 - 7:00 PM</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>Mon</div>
+                <div className='cell'>Tue</div>
+                <div className='cell'>Wed</div>
+                <div className='cell'>Thu</div>
+                <div className='cell'>Fri</div>
+                <div className='cell'>Sat</div>
+                <div className='cell'>Sun</div>
+              </div>
+              <div className='row'>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>MAT</div>
+                <div className='cell'>C + T</div>
+              </div>
+            </div>
           </div>
-          <div className='col'>
-            <div className='row'>Time</div>
-            <div className='row'>5:00-6:00 PM</div>
-            <div className='row'>6:00-7:00 PM</div>
-            <div className='row'>5:00-6:00 PM</div>
-            <div className='row'>6:00-7:00 PM</div>
-            <div className='row'>5:00-6:00 PM</div>
-            <div className='row'>6:00-7:00 PM</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Monday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Biology</div>
-            <div className='row'>Biology</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Tuesday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Biology</div>
-            <div className='row'>Biology</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Wednesday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Chemistry</div>
-            <div className='row'>Chemistry</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Thursday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Chemistry</div>
-            <div className='row'>Chemistry</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Friday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Physics</div>
-            <div className='row'>Physics</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Saturday</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Science</div>
-            <div className='row'>Maths</div>
-            <div className='row'>Physics</div>
-            <div className='row'>Physics</div>
-            <div className='row'>Maths</div>
-          </div>
-          <div className='col'>
-            <div className='row'>Sunday</div>
-            <div className='row'>Class + Test</div>
-            <div className='row'>Class + Test</div>
-            <div className='row'>Class + Test</div>
-            <div className='row'>Class + Test</div>
-            <div className='row'>Class + Test</div>
-            <div className='row'>Class + Test</div>
-          </div>
-        </div>
       </div>
       <ContainerRight state={state} dispatch={dispatch} />
     </div>
--- src/pages/twelfth/twelfth.js
+++ src/pages/twelfth/twelfth.js
@@ -6,71 +6,104 @@ const Twelfth = ({ state, dispatch }) => {
     <div className='container'>
     <div className='container-left'>
       <div className='title'>Time Table for Class 11<sup>th</sup> and 12<sup>th</sup></div>
-      <div className='twelfth'>
-        <div className='col'>
-          <div className='row'>Class</div>
-          <div className='row'>11th</div>
-          <div className='row'>11th</div>
-          <div className='row'>12th</div>
-          <div className='row'>12th</div>
+        <div className='twelfth'>
+          <div className='col'>
+            <div className='row'>
+              <div className='cell'>Class 11th</div>
+              <div className='cell'>Time 6:00 - 7:30 AM</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>Mon</div>
+              <div className='cell'>Tue</div>
+              <div className='cell'>Wed</div>
+              <div className='cell'>Thu</div>
+              <div className='cell'>Fri</div>
+              <div className='cell'>Sat</div>
+              <div className='cell'>Sun</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>PHY</div>
+              <div className='cell'>CHE</div>
+              <div className='cell'>PHY</div>
+              <div className='cell'>CHE</div>
+              <div className='cell'>PHY</div>
+              <div className='cell'>CHE</div>
+              <div className='cell'>C + T</div>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='row'>
+              <div className='cell'>Class 11th</div>
+              <div className='cell'>Time 5:00 - 7:00 PM</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>Mon</div>
+              <div className='cell'>Tue</div>
+              <div className='cell'>Wed</div>
+              <div className='cell'>Thu</div>
+              <div className='cell'>Fri</div>
+              <div className='cell'>Sat</div>
+              <div className='cell'>Sun</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>MAT</div>
+              <div className='cell'>BIO</div>
+              <div className='cell'>MAT</div>
+              <div className='cell'>BIO</div>
+              <div className='cell'>MAT</div>
+              <div className='cell'>BIO</div>
+              <div className='cell'>C + T</div>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='row'>
+              <div className='cell'>Class 12th</div>
+              <div className='cell'>Time 6:00 - 7:30 AM</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>Mon</div>
+              <div className='cell'>Tue</div>
+              <div className='cell'>Wed</div>
+              <div className='cell'>Thu</div>
+              <div className='cell'>Fri</div>
+              <div className='cell'>Sat</div>
+              <div className='cell'>Sun</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>CHE</div>
+              <div className='cell'>PHY</div>
+              <div className='cell'>CHE</div>
+              <div className='cell'>PHY</div>
+              <div className='cell'>CHE</div>
+              <div className='cell'>PHY</div>
+              <div className='cell'>C + T</div>
+            </div>
+          </div>
+          <div className='col'>
+            <div className='row'>
+              <div className='cell'>Class 12th</div>
+              <div className='cell'>Time 5:00 - 7:00 PM</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>Mon</div>
+              <div className='cell'>Tue</div>
+              <div className='cell'>Wed</div>
+              <div className='cell'>Thu</div>
+              <div className='cell'>Fri</div>
+              <div className='cell'>Sat</div>
+              <div className='cell'>Sun</div>
+            </div>
+            <div className='row'>
+              <div className='cell'>BIO</div>
+              <div className='cell'>MAT</div>
+              <div className='cell'>BIO</div>
+              <div className='cell'>MAT</div>
+              <div className='cell'>BIO</div>
+              <div className='cell'>MAT</div>
+              <div className='cell'>C + T</div>
+            </div>
+          </div>
         </div>
-        <div className='col'>
-          <div className='row'>Time</div>
-          <div className='row'>6:00-7:30 AM</div>
-          <div className='row'>5:00-7:00 PM</div>
-          <div className='row'>6:00-7:30 AM</div>
-          <div className='row'>5:00-7:00 PM</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Monday</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Tuesday</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Wednesday</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Thursday</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Friday</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Saturday</div>
-          <div className='row'>Chemistry</div>
-          <div className='row'>Biology</div>
-          <div className='row'>Physics</div>
-          <div className='row'>Maths</div>
-        </div>
-        <div className='col'>
-          <div className='row'>Sunday</div>
-          <div className='row'>Class + Test</div>
-          <div className='row'>Class + Test</div>
-          <div className='row'>Class + Test</div>
-          <div className='row'>Class + Test</div>
-        </div>
-      </div>
     </div>
       <ContainerRight state={state} dispatch={dispatch} />
     </div>
